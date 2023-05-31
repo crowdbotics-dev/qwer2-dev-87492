0513f91fa8adc16758d665c153f4e4e266c027e3
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-31 10:43:51 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Scxvx
+admin.site.register(Scxvx)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Scxvx(models.Model):
+    'Generated Model'
+    bcbhd = models.BigIntegerField()
